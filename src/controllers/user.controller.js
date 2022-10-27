1332ea4129bda9fb64708e55b25916c337ffb31c
lobotelho22 <eduardo.ocara@gmail.com>
2022-10-27 19:35:52 -0300

    implementado service que gera o token no POST de endpoint /login

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,13 +4,17 @@ const doLogin = async (req, res) => {
     try {
         const { email, password } = req.body;
 
-        let error = await UserService.checkLogin(email, password);
+        const error = await UserService.checkLogin(email, password);
         if (error.type) { return res.status(400).json({ message: error.message }); }
         
-        error = await UserService.getByEmailAndPassword(email, password);
-        if (error.type) { return res.status(400).json({ message: error.message }); }
+        const userDataVal = await UserService.getByEmailAndPassword(email, password);
+        if (userDataVal.type) { return res.status(400).json({ message: userDataVal.message }); }
         
-        return res.status(200).json({ message: `email: ${email} & password: ${password} ` });
+        const userData = userDataVal.message;
+
+        const token = UserService.getToken(userData);
+
+        return res.status(200).json({ token });
     } catch (error) {
         console.log(error.message);
         res.status(500).json({ message: 'Xiii, algo deu errado...' });
--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,3 +1,6 @@
+require('dotenv/config');
+const jwt = require('jsonwebtoken');
+
 const loginValidation = require('./validations/validationLogin');
 const { User } = require('../models');
 
@@ -16,7 +19,24 @@ const getByEmailAndPassword = async (email, password) => {
   return { type: null, message: user.dataValues }; 
 };
 
+const getToken = (userData) => {
+  const secret = process.env.JWT_SECRET;
+  const jwtConfig = {
+    expiresIn: '30d',
+    algorithm: 'HS256',
+  };
+
+  const token = jwt.sign({
+    data: { userId: userData.id, email: userData.email },
+    },
+    secret,
+    jwtConfig);
+  console.log(token);
+  return token;
+};
+
 module.exports = {
   checkLogin,
   getByEmailAndPassword,
+  getToken,
 };
