ae044ca5e13fd7634324446a52de27683747f3cf
lobotelho22 <eduardo.ocara@gmail.com>
2022-10-27 15:51:16 -0300

    criada a função -- sem a lógica -- doLogin, para o endpoint POST /login

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,6 +1,7 @@
 const doLogin = (req, res) => {
     try {
-        console.log('teste');
+        const { email, password } = req.body;
+        res.status(200).json({ message: `email: ${email} & password: ${password} ` });
     } catch (error) {
         console.log(error.message);
         res.status(500).json({ message: 'Xiii, algo deu errado...' });
