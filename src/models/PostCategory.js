97c2dc487af956050c32baaa9e75c8e710295545
Eduardo Santos <eduardo.ocara@gmail.com>
2022-10-29 10:18:56 -0300

    implementado model PostCategory, fazendo a relação intermediárias entre os models BlogPost e Category, com relação N:N

---
+++ src/models/PostCategory.js
@@ -0,0 +1,30 @@
+const PostCategoryModel = (sequelize, DataTypes) => {
+  const PostCategory = sequelize.define('PostCategory',
+  {
+    postId: { type: DataTypes.INTEGER, foreignKey: true },
+    categoryId: { type: DataTypes.INTEGER, foreignKey: true },
+  },
+  {
+    underscored: true,
+    timestamps: false,
+  });
+
+  PostCategory.associate = (models) => {
+    models.Category.belongsToMany(models.BlogPost,{
+      as: 'blog_post',
+      through: PostCategory,
+      foreignKey: 'categoryId',
+      otherKey: 'postId',
+    });
+    models.BlogPost.belongsToMany(models.Category, {
+      as: 'categories',
+      through: PostCategory,
+      foreignKey: 'postId',
+      otherKey: 'categoryId',
+    });
+  }
+
+  return PostCategory;
+};
+
+module.exports = PostCategoryModel;
